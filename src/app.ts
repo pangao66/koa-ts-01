2efc2e8627617ef4c721272aec5204a0d19a4da5
pangao <pangao@tuhu.cn>
2020-07-24 20:42:00 +0800

    添加接口

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import Koa from 'koa'
 import koaBodyParser from 'koa-bodyparser'
+import KoaStatic from 'koa-static'
+import path from 'path'
 
 const app = new Koa()
 import router from './routes/index'
 
+const staticPath = './static'
+console.log(__dirname)
+app.use(KoaStatic(
+  path.join(__dirname, staticPath)
+))
 app.use(koaBodyParser())
 app.use(router())
 app.listen(3000)
+
